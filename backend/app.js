32369577f3826cb44fcfc3e166c2480feaf1a801
Victoria Kee <victoriakee95@gmail.com>
2021-06-01 14:17:03 -0500

    Style result msg

--- backend/app.js
+++ backend/app.js
@@ -3,11 +3,10 @@ let computerScore = 0;
 const userScore_span = document.getElementById('user-score');
 const computerScore_span = document.getElementById('comp-score');
 const scoreBoard_div = document.querySelector('.score-board');
-const result_div = document.querySelector('.result');
+const outcome = document.querySelector('.result');
 const rock_div = document.getElementById('r');
 const paper_div = document.getElementById('p');
 const scissors_div = document.getElementById('s');
-const outcome = document.getElementById('outcome');
 
 function getComputerChoice() {
     const choices = ['r', 'p', 's'];
